b7168778c2ea47d7dd235b20108f6e3a815c6968
Cristopher <clopez-2021325@kinal.edu.gt>
2026-07-27 22:34:25 -0600

    feat: interfaz general

---
+++ flotasegura-ts/src/utils/readline.ts
@@ -0,0 +1,20 @@
+import readline from "readline";
+
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+
+export function preguntar(pregunta: string): Promise<string> {
+  return new Promise((resolve) => {
+    rl.question(pregunta, (respuesta) => {
+      resolve(respuesta.trim());
+    });
+  });
+}
+
+export function cerrarLectura(): void {
+  rl.close();
+}
